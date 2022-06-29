7c0d7438da318b429cd3085372e2c4be8da6d84d
Jong <60678795+onghyuk@users.noreply.github.com>
2022-06-29 21:08:13 +0900

    --

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const bt1 = document.querySelector("#bt1");
 const bt2 = document.querySelector("#bt2");
+const bt3 = document.querySelector("#bt3");
+const bt4 = document.querySelector("#bt4");
+
 
 const img1 = document.querySelector("#img1");
 const img2 = document.querySelector("#img2");
+const img3 = document.querySelector("#img3");
+const img4 = document.querySelector("#img4");
+
 
 
 
@@ -14,5 +20,17 @@ active2 = () => {
     img2.classList.toggle("active");
 }
 
+active3 = () => {
+    img3.classList.toggle("active");
+}
+
+active4 = () => {
+    img4.classList.toggle("active");
+}
+
 bt1.addEventListener("click", active);
 bt2.addEventListener("click", active2);
+bt3.addEventListener("click", active3);
+bt4.addEventListener("click", active4);
+
+

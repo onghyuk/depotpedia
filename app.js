d956d1f3c7bb7204574af2fdce07506b122e81eb
Jong <60678795+onghyuk@users.noreply.github.com>
2022-06-29 20:22:09 +0900

    --

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const potato = document.querySelector(".button");
-const img = document.querySelector("#img1");
+const bt1 = document.querySelector("#bt1");
+const bt2 = document.querySelector("#bt2");
+
+const img1 = document.querySelector("#img1");
+const img2 = document.querySelector("#img2");
+
 
 
 active = () => {
-    img.classList.toggle("active");
+    img1.classList.toggle("active");
 }
 
+active2 = () => {
+    img2.classList.toggle("active");
+}
 
-potato.addEventListener("click", active);
+bt1.addEventListener("click", active);
+bt2.addEventListener("click", active2);
